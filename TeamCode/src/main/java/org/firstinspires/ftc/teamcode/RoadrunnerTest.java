e49b2f51123df239a42b0c3e71d8f1bac9bf7be0
ernest <ernest@levkovit.ch>
2024-11-16 12:23:32 +0100

    Prototype Roadrunner implementation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadrunnerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadrunnerTest.java
@@ -12,35 +12,6 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.Roadrunner.MecanumDrive;
 
-class DriveTrain {
-    public DcMotorEx backLeftMotor;
-    public DcMotorEx backRightMotor;
-    public DcMotorEx frontLeftMotor;
-    public DcMotorEx frontRightMotor;
-
-    public DcMotorEx leftOdo;
-    public DcMotorEx middleOdo;
-    public DcMotorEx rightOdo;
-
-    public DriveTrain(MyHardwareMap hMap) {
-        backLeftMotor = hMap.backLeftMotor;
-        backRightMotor = hMap.backRightMotor;
-        frontLeftMotor = hMap.frontLeftMotor;
-        frontRightMotor = hMap.frontRightMotor;
-
-        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        leftOdo.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        middleOdo.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightOdo.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftOdo.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        middleOdo.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rightOdo.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    }
-}
 @Autonomous(name="ROAD RUNNER TEST")
 public class RoadrunnerTest extends LinearOpMode {
     @Override
